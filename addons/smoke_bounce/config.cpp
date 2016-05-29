91a8eff2786a99b08b03b400da779c9ae7d9337a
freghar <freghar@dummy.tld>
2016-05-29 23:04:53 +0200

    add smoke_bounce, an UGL smoke bounce reducer
    Signed-off-by: freghar <freghar@dummy.tld>

---
+++ addons/smoke_bounce/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches {
+    class Smoke_Bounce {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {"A3_Weapons_F"};
+    };
+};
+
+class CfgAmmo {
+    class SmokeShell;
+    class G_40mm_Smoke : SmokeShell {
+        /* inherited from SmokeShell is shotSmokeX which ignores
+         * "deflecting" and "deflectionSlowDown" parameters */
+        simulation = "shotSmoke";
+        /* multiplier; small bounce, on 400m flat ground shot ~10m bounce */
+        deflectionSlowDown = 0.3;
+    };
+};
