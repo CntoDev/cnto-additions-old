6548e0979391bde703b996d3bdb1118c3abc3a87
freghar <freghar@dummy.tld>
2016-06-15 00:27:31 +0200

    add CNTO logo
    Signed-off-by: freghar <freghar@dummy.tld>

--- mod.cpp
+++ mod.cpp
@@ -2,3 +2,5 @@ name = "CNTO Additions";
 author = "CNTO RnD";
 hideName = 0;
 overview = "Various small custom additions for the CNTO Arma community, by multiple authors.";
+picture = "cnto_logo.paa";
+logo = "cnto_logo.paa";
