a35e799137c5f7693dd8c924405cbb439e441a62
freghar <freghar@dummy.tld>
2016-08-08 04:40:04 +0200

    import all individual tweaks from cnto-additions
    Signed-off-by: freghar <freghar@dummy.tld>

---
+++ addons/flare_intensity/config.cpp
@@ -0,0 +1,38 @@
+/*
+ * flare intensity - increase brightness/intensity of flares, so they
+ * actually can illuminate something (even without NVGs!)
+ * - taken from https://feedback.bistudio.com/T76756
+ */
+
+class CfgPatches {
+    class Flare_Intensity {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {
+            "A3_Weapons_F",
+            "ace_grenades"
+        };
+    };
+};
+
+class CfgAmmo {
+    class FlareCore;
+    class FlareBase;
+    class Flare_82mm_AMOS_White: FlareCore {
+        timeToLive = 80;
+        brightness = 200;
+        intensity  = 5000000;
+    };
+    class F_40mm_White: FlareBase {
+        brightness = 120;
+        intensity  = 5000000;
+    };
+    class F_20mm_White: FlareBase {
+        brightness = 80;
+        intensity  = 5000000;
+    };
+    class F_Signal_Green: FlareBase {
+        brightness = 80;
+        intensity  = 5000000;
+    };
+};
