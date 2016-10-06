237f1947fc5fdbf5214a5b8532ce5987f172be08
freghar <freghar@dummy.tld>
2016-10-06 03:30:12 +0200

    lighting_fixes: make some RHS flashlights usable as ACE map lights
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/lighting_fixes/config.cpp
+++ addons/lighting_fixes/config.cpp
@@ -9,7 +9,9 @@ class CfgPatches {
         weapons[] = {};
         requiredAddons[] = {
             "A3_Weapons_F",
-            "ace_grenades"  //overwrites flare intensity
+            "ace_grenades",     // overwrites flare intensity
+            "rhs_c_weapons",    // rhs_acc_2dpZenit
+            "rhsusf_c_weapons"  // rhsusf_acc_M952V
         };
     };
 };
@@ -38,5 +40,43 @@ class CfgAmmo {
     class F_Signal_Green: FlareBase {
         brightness = 80;
         intensity  = 5000000;
+    };
+};
+
+class CfgWeapons {
+    /*
+     * make some RHS flashlights behave/weight as much as ACE map flashlights
+     */
+    class InventoryFlashLightItem_Base_F;
+    class acc_flashlight;
+    class rhs_acc_2dpZenit : acc_flashlight {
+        class ItemInfo : InventoryFlashlightItem_Base_F {
+            mass = 1;
+            class FlashLight {
+                ACE_Flashlight_Colour = "white";
+                ACE_Flashlight_Beam = "\z\ace\addons\map\UI\Flashlight_beam_white_ca.paa";
+                ACE_Flashlight_Size = 2.5;
+                ACE_Flashlight_Sound = 1;
+            };
+        };
+    };
+    class acc_pointer_IR;
+    class rhsusf_acc_anpeq15 : acc_pointer_IR {
+        class ItemInfo;
+    };
+    class rhsusf_acc_anpeq15_light : rhsusf_acc_anpeq15 {
+        class ItemInfo : ItemInfo {
+            class Flashlight {
+                ACE_Flashlight_Colour = "white";
+                ACE_Flashlight_Beam = "\z\ace\addons\map\UI\Flashlight_beam_white_ca.paa";
+                ACE_Flashlight_Size = 2.5;
+                ACE_Flashlight_Sound = 1;
+            };
+        };
+    };
+    class rhsusf_acc_M952V : rhsusf_acc_anpeq15_light {
+        class ItemInfo : ItemInfo {
+            mass = 1;
+        };
     };
 };
