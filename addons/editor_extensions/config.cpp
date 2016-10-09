ea2f3932f3a5e93a307398e08f927955baeeb8d5
freghar <freghar@dummy.tld>
2016-10-09 18:40:35 +0200

    editor_extensions: add the Move respawn module
    Useful for 3D positioning of respawn modules, ie. on a ship
    or inside a specific floor of a building.
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/editor_extensions/config.cpp
+++ addons/editor_extensions/config.cpp
@@ -24,6 +24,7 @@ class CfgFunctions {
             class m_instaOSD;
             class m_arsenal;
             class m_teleportOnJIP;
+            class m_moveRespawn;
         };
         class misc {
             file = "\editor_extensions\functions\misc";
--- addons/editor_extensions/modules.h
+++ addons/editor_extensions/modules.h
@@ -330,3 +330,22 @@ class a3ee_teleport_on_jip : a3ee_module_base {
         class teleport { init = "if (isServer) then { (_this select 0) call a3ee_fnc_m_teleportOnJIP }"; };
     };
 };
+
+class a3ee_move_respawn : a3ee_module_base {
+    scope = 2;
+    icon = "\a3\Missions_F_Curator\data\img\iconMPTypeSectorControl_ca.paa";
+    displayName = "Move respawn";
+    class Attributes {
+        class movemarkers {
+            property = "a3ee_movemarkers";
+            control = "EditArray";
+            displayName = "Markers";
+            expression = "_this setVariable [""%s"",_value]";
+            defaultValue = "[""respawn""]";
+            tooltip = "Comma-separated list of marker names to move to this module's 3D position on mission start. The nearest surface below this position will be used on respawn.";
+        };
+    };
+    class EventHandlers : EventHandlers {
+        class teleport { init = "if (isServer) then { (_this select 0) call a3ee_fnc_m_moveRespawn }"; };
+    };
+};
