88ad7f61b68598d1db8db925b792abb6818f4a3f
freghar <freghar@dummy.tld>
2017-06-22 21:18:42 +0200

    editor_extensions: remove Apex object checking (useless on v1.72+)
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/editor_extensions/menu/config.cpp
+++ addons/editor_extensions/menu/config.cpp
@@ -10,7 +10,6 @@ class CfgFunctions {
     class a3ee {
         class menu {
             file = "\editor_extensions\menu\functions";
-            class checkApex;
             class checkOverload;
             class fillRoleDesc;
         };
@@ -24,15 +23,9 @@ class display3DEN {
             class Items {
                 class A3EE {
                     items[] += {
-                        "A3EE_Check_Apex",
                         "A3EE_Check_Overload",
                         "A3EE_Fill_Role_Desc"
                     };
-                };
-                class A3EE_Check_Apex {
-                    text = "Check for Apex objects";
-                    picture = "\a3\Modules_F_Curator\Data\portraitordnancerocket_ca.paa";
-                    action = "collect3DENHistory { [] call a3ee_fnc_checkApex }";
                 };
                 class A3EE_Check_Overload {
                     text = "Check for overloaded units";
