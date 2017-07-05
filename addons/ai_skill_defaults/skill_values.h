e1e4482aae0d6539d620eb4ad78976531d7de211
freghar <freghar@dummy.tld>
2017-07-05 05:33:04 +0200

    ai_skill_defaults: make spotting slower, aim faster
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/ai_skill_defaults/skill_values.h
+++ addons/ai_skill_defaults/skill_values.h
@@ -3,11 +3,11 @@
  */
 [
     [  "aimingAccuracy",  0.2  ],
-    [  "aimingShake",     0.2  ],
-    [  "aimingSpeed",     0.9  ],
+    [  "aimingShake",     1.0  ],
+    [  "aimingSpeed",     0.95 ],
     [  "endurance",       1.0  ],
     [  "spotDistance",    0.8  ],
-    [  "spotTime",        0.9  ],
+    [  "spotTime",        0.7  ],
     [  "courage",         1.0  ],
     [  "reloadSpeed",     1.0  ],
     [  "commanding",      1.0  ],
