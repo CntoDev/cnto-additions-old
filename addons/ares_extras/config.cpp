f479fad6c540da6ca8cd7228ae0348d74182ca46
freghar <freghar@dummy.tld>
2017-07-07 03:29:04 +0200

    ares_extras: redesign for Achilles, drop old functions
    * removed "Force WP behaviour", shouldn't be needed without ASR AI, but if you do need it, just disable AUTOCOMBAT temporarily
    * "Unload cargo in combat" on/off renamed to "No unload in combat" (disable-only) as the default is enabled
    * "AE - AI Features" removed completely, use "AI Behaviour" -> "Change abilities"
    * removed "Force vehicle lights" - just double-click on any vehicle and you'll see light on/off icons
    * removed "Suppress (manual, ~90sec)", use "Fire Support" -> "Create suppression target" and "Suppressive Fire", .. or "ACE AI" -> "Suppressive Fire"
    * removed "Suppress (bis, ~90sec)", shouldn't be needed without ASR AI as units now engage at vast distances as long as they know about the target
    * simplified "Suppress (bis)", now works exactly like the doSuppressiveFire scripting command with all its bugs, .. prefer use only on units, not on ground ... note that you really shouldn't need to use this command
    * added "Reveal enemies", see further below for how to use it - this should be your replacement for "Suppress (bis)"
    * moved "Allow Crew In Immobile", "Forget enemies", "Reveal enemies", "Flee", "No unload in combat" to "AI Behaviour"
    * removed "Flashlights/lasers", use "Equipment" -> "NVD/Tactical..." to change equipment and "Tactical Light/IR Laser On/Off" to force on/off flashlight/IR
    * removed "Reset anim state"
    * moved "Locality - Get", "Locality - Set", "Give Zeus to player", "Set player insignia" to "Development Tools"
    * removed "Align to Surface Level" and "Align to Surface Vector" (were mostly for MCC-loaded content)
    * moved "Terrain Objects - Hide", "Terrain Objects - Show" to "Environment"
    * moved "End Mission - Won", "End Mission - Lost" to "Scenario Flow"
    * ...
    * removed all vanilla stuff from "Respawn", it was for a public-server-style gameplay and unrelated to how we do respawn
    * moved everything "AE - Respawn" to "Respawn"
    * ...
    * removed "ACE FullHeal", use "ACE Medical" -> "Heal"
    * removed "Captive - On/Off", generally useless, could be used to make a unit "civilian" without fully changing sides, but could have broken ACE captive functionality, so juse use "Player" -> "Change side"
    * removed "Curator - Add/Remove", use "Zeus" -> "Add/remove editable objects" ... or "ACE Utility" -> "Update Editable Objects"
    * moved "Delete units (really)" to "Objects"
    * removed "Immortal - On/Off", use "Objects" -> "Make invincible"
    * removed "Simulation - On/Off", use "Objects" -> "Enable/Disable simulation"
    * removed "Teleport*", use "Player" -> "Teleport" ... or "ACE Utility" -> "Teleport Players"
    * ...
    * all of ares_extras internals re-written to use native Achilles functions, making it hopefully much easier to maintain
    * all of [U], [G] and [P] redesigned to use Achilles selection system (Enter to confirm, Escape to cancel)
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/ares_extras/config.cpp
+++ addons/ares_extras/config.cpp
@@ -2,36 +2,30 @@ class CfgPatches {
     class Ares_Extras {
         units[] = {};
         weapons[] = {};
-        requiredAddons[] = {};
+        requiredAddons[] = {
+            "A3_Modules_F_Curator_Respawn"
+        };
     };
 };
 
 class CfgFunctions {
     class Ares_Extras {
         class All {
-            file = "\ares_extras\functions";
+            file = "\ares_extras";
             class init { postInit = 1; };
-            class Dialog;
-            class ForGroups;
-            class ForGroupsMP;
-            class ForUnits;
-            class ForUnitsMP;
-            class Selection;
-            class StdDistances;
-        };
-        /* these came from the original A3MT */
-        class A3MT {
-            file = "\ares_extras\functions\a3mt";
-            class teleport;
-            class addRemoveNVGs;
-            class vehLightOn;
-            class setPosAGLS;
-        };
-        /* these provide A3MT-like functionality, but didn't come from it */
-        class New {
-            file = "\ares_extras\functions\new";
-            class manualSuppress;
-            class bisSuppress;
         };
     };
+};
+
+class CfgVehicles {
+    /*
+     * hide all vanilla stuff in Respawn, related to public-server-style-respawn
+     */
+    class Module_F;
+	class ModuleRespawnInventory_F : Module_F {
+		scopeCurator = 0;
+	};
+	class ModuleRespawnPositionWest_F : Module_F {
+		scopeCurator = 0;
+	};
 };
