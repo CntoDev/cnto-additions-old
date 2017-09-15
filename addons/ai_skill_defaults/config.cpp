934c91e2b995b47998256640f3cc9cf7727fd4ba
CNTODachi <dene_ford@yahoo.com>
2017-09-15 08:46:09 +0100

    AI Skill Updates
    - Removed warning.
    - Renoved Custom Skill Definition.

--- addons/ai_skill_defaults/config.cpp
+++ addons/ai_skill_defaults/config.cpp
@@ -47,20 +47,6 @@ class CfgVehicles {
 };
 #endif
 
-/* linear X=Y from 0.2 up, don't ask how, took 3 days of research */
-class CfgAISkill {
-    aimingAccuracy[] = {0,0, 0.5,0, 1,1};
-    aimingShake[]    = {0,0, 0.5,0, 1,1};
-    aimingSpeed[]    = {0,0, 0.5,0, 1,1};
-    endurance[]      = {0,0, 0.5,0, 1,1};
-    spotDistance[]   = {0,0, 0.5,0, 1,1};
-    spotTime[]       = {0,0, 0.5,0, 1,1};
-    courage[]        = {0,0, 0.5,0, 1,1};
-    reloadSpeed[]    = {0,0, 0.5,0, 1,1};
-    commanding[]     = {0,0, 0.5,0, 1,1};
-    general[]        = {0,0, 0.5,0, 1,1};
-};
-
 class Cfg3DEN {
     class Attributes {
         class Slider;
