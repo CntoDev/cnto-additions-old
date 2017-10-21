1c31d50435c3ad2034da4ac707375c9b06b96c50
freghar <freghar@dummy.tld>
2017-10-21 03:10:45 +0200

    editor_extensions: add ACRE _ID_ unique radios check to menu
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/editor_extensions/menu/config.cpp
+++ addons/editor_extensions/menu/config.cpp
@@ -12,6 +12,7 @@ class CfgFunctions {
             file = "\editor_extensions\menu\functions";
             class checkOverload;
             class fillRoleDesc;
+            class checkAcreIds;
         };
     };
 };
@@ -23,8 +24,24 @@ class display3DEN {
             class Items {
                 class A3EE {
                     items[] += {
-                        "A3EE_Check_Overload",
                         "A3EE_Fill_Role_Desc"
+                    };
+                };
+                class A3EE_Fill_Role_Desc {
+                    text = "Fill in Role Description";
+                    picture = "\a3\Modules_F_Curator\Data\portraitmissionname_ca.paa";
+                    action = "collect3DENHistory { [] call a3ee_fnc_fillRoleDesc }";
+                };
+
+                class Tools {
+                    items[] += { "A3EE_Compositions" };
+                };
+                class A3EE_Compositions {
+                    text = "A3EE Compositions Checks";
+                    picture = "\a3\3DEN\Data\Displays\Display3DEN\PanelRight\side_custom_ca.paa";
+                    items[] = {
+                        "A3EE_Check_Overload",
+                        "A3EE_Check_ACRE_IDs"
                     };
                 };
                 class A3EE_Check_Overload {
@@ -32,10 +49,10 @@ class display3DEN {
                     picture = "\a3\Modules_F_Curator\Data\portraitrespawninventory_ca.paa";
                     action = "collect3DENHistory { [] call a3ee_fnc_checkOverload }";
                 };
-                class A3EE_Fill_Role_Desc {
-                    text = "Fill in Role Description";
-                    picture = "\a3\Modules_F_Curator\Data\portraitmissionname_ca.paa";
-                    action = "collect3DENHistory { [] call a3ee_fnc_fillRoleDesc }";
+                class A3EE_Check_ACRE_IDs {
+                    text = "Check for unique ACRE2 radios";
+                    picture = "\a3\Modules_F_Curator\Data\portraitradio_ca.paa";
+                    action = "collect3DENHistory { [] call a3ee_fnc_checkAcreIds }";
                 };
             };
         };
