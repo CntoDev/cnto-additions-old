59c0bd82dadc46f1ebe725cc27aa7fa676eaf82f
Milivoj Milani <milanimilivoj@gmail.com>
2017-11-21 10:53:52 +0100

    adds cnto subcategories
    Adds CNTO related composition subcategories help organize.

---
+++ addons/editor_extensions/custom_subcategories/config.cpp
@@ -0,0 +1,36 @@
+/*
+ * adds CNTO composition category and subcategories to keep things organized
+ */
+
+class CfgPatches {
+    class Eden_Extensions_Custom_Subcategories {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {
+            "A3_3DEN"
+        };
+    };
+};
+
+class CfgEditorSubcategories
+{
+	class CNTODefaults // Category class, you point to it in editorSubcategory property
+	{
+		displayName = "CNTO Defaults"; // Name visible in the list
+	};
+	
+	class CNTOStandardFaction // Category class, you point to it in editorSubcategory property
+	{
+		displayName = "CNTO Standard Faction"; // Name visible in the list
+	};	
+	
+	class CNTOStandardFactionAdaptation // Category class, you point to it in editorSubcategory property
+	{
+		displayName = "CNTO Standard Faction Adaptation"; // Name visible in the list
+	};	
+	
+	class CNTOCustomFaction // Category class, you point to it in editorSubcategory property
+	{
+		displayName = "CNTO Custom Faction"; // Name visible in the list
+	};		
+};
