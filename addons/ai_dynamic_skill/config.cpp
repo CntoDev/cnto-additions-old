e37250cf25b68582a5dd6f3d1cc31594d6909d9d
freghar <freghar@dummy.tld>
2018-03-06 14:37:59 +0100

    ai_dynamic_skill: revert UI sliders back to vanilla 0.2-1.0 range
    The range modification was done mainly for debugging purposes and while
    having access to 0 skill value may make some sense sometimes (ie. unit
    that never hits with bullets, even on 1m distance), it probably doesn't
    make sense to have it UI-accessible as it can lead to Zeus mistakenly
    setting it.
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/ai_dynamic_skill/config.cpp
+++ addons/ai_dynamic_skill/config.cpp
@@ -72,7 +72,13 @@ class CfgVehicles {
 };
 #endif
 
-/* linear X=Y from 0.2 up, don't ask how, took 3 days of research */
+/*
+ * linear X=Y from 0.2 up, don't ask how, took 3 days of research
+ * https://drive.google.com/open?id=1GDzxZpVa1IDFP9GAeHUVrKIiGqKR5jv68qvE-0JhMQ0
+ *
+ * default UI slider ranges in Eden / Zeus are 0.2 to 1.0, so the user won't set
+ * values below 0.2 via UI
+ */
 class CfgAISkill {
     aimingAccuracy[] = {0,0, 0.5,0, 1,1};
     aimingShake[]    = {0,0, 0.5,0, 1,1};
@@ -84,20 +90,4 @@ class CfgAISkill {
     reloadSpeed[]    = {0,0, 0.5,0, 1,1};
     commanding[]     = {0,0, 0.5,0, 1,1};
     general[]        = {0,0, 0.5,0, 1,1};
-};
-
-class Cfg3DEN {
-    class Attributes {
-        class Slider;
-        class Controls;
-        class Value;
-        /* allow full 0-1 setting, not just 0.2-1 */
-        class Skill : Slider {
-            class Controls : Controls {
-                class Value : Value {
-                    sliderRange[] = {0, 1};
-                };
-            };
-        };
-    };
 };
