f448fc20607aa43b5392375a2c3a8b9c465f1b5a
freghar <freghar@dummy.tld>
2018-04-07 21:46:39 +0200

    static_line_chute: parachutes, automated
    Static line:
    
    - done as ACE menu item visible to crew (pilot, copilot, gunner,
      commander, but not to passengers)
    
    - the ACE menu item shows only in airplanes and helicopters with enough
      seats (9 or more "passenger" spaces, which confusingly includes
      copilot (not a pilot/driver) and crew chief (not gunner))
      https://i.imgur.com/4jiNPyQ.jpg
    
    - the ACE menu item is also limited to >100m for safety reasons
    
    - ejects all passengers (in cargo and "turrets" - units able to shoot
      with small arms) or individual groups
    
    - ejection order is cargo index order (vehicle boarding order),
      but reversed (LIFO)
    
    - delay between ejected units depends on speed (see code)
    
    - handcuffed and unconscious units are ignored
    
    - ejection progress is indicated via chat to all units still inside
      the vehicle, the ACE menu item is disabled for the duration of the
      deployment (to prevent accidents)
    
    RHS D6 parachute changes:
    
    - adjusted to fall more vertically (like a dome chute should) instead of
      gliding like vanilla steerable parachute
    
    - reserve parachute disabled as ACE Reserve Parachute is identical to
      vanilla steerable parachute and for ease of use (so that landed units
      don't carry one on their back)
    
    Signed-off-by: freghar <freghar@dummy.tld>

---
+++ addons/static_line_chute/config.cpp
@@ -0,0 +1,35 @@
+/*
+ * various Arma 3 Spectator related tweaks and configuration
+ */
+
+class CfgPatches {
+    class Static_Line_Chute {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { "rhs_c_troops" };
+    };
+};
+
+class CfgVehicles {
+    /* make D6 fall more vertically than a glider */
+    class Steerable_Parachute_F;
+    class rhs_d6_Parachute : Steerable_Parachute_F {
+        ace_hasReserveParachute = 0;
+        ace_reserveParachute = "";
+        airInfluence = 0.1;
+    };
+};
+
+class CfgFunctions {
+    class Static_Line_Chute {
+        class All {
+            file = "\static_line_chute";
+            class createACEAction;
+            class addActionToVehicles { postInit = 1; };
+            class getPassengers;
+            class ejectWithChute;
+            class ejectUnits;
+            class vehicleChat;
+        };
+    };
+};
