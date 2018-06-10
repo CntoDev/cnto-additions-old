f4ddaa8c06a78a5d534f160f7a1b8775912d5491
freghar <freghar@dummy.tld>
2018-06-10 22:10:26 +0200

    extra_ace_unconscious: add more constrains to units waking up
    1)
    Wait for pain levels to reduce below 0.4. This prevents significantly
    injured units from waking up within a few seconds. Additionally, if
    painIsOnlySuppressed ACE setting is active, morphine will not wake
    the unit up - the pain levels need to lower naturally (max. within
    a few minutes).
    
    2)
    Don't wake the unit up while it has an active head injury.
    
    Signed-off-by: freghar <freghar@dummy.tld>

---
+++ addons/misc_tiny_configs/extra_ace_unconscious/config.cpp
@@ -0,0 +1,22 @@
+/*
+ * extend ACE3 unconsciousness logic
+ */
+
+class CfgPatches {
+    class Extra_ACE_Unconscious {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {
+            "ace_medical"
+        };
+    };
+};
+
+class CfgFunctions {
+    class Extra_ACE_Unconscious {
+        class All {
+            file = "\misc_tiny_configs\extra_ace_unconscious";
+            class init { postInit = 1; };
+        };
+    };
+};
