b12fb8072f429d4c99d6aa22d32dde85151eb2f6
freghar <freghar@dummy.tld>
2018-08-14 20:02:55 +0200

    recoil_tweaks: remove NIArms modifications
    Maybe temporarily - for now, the dependency was just not worth it.
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/misc_tiny_configs/recoil_tweaks/config.cpp
+++ addons/misc_tiny_configs/recoil_tweaks/config.cpp
@@ -5,9 +5,7 @@ class CfgPatches {
         requiredAddons[] = {
             "A3_Weapons_F",
             "rhsusf_c_weapons",
-            "rhs_c_weapons",
-            "hlcweapons_m60e4",
-            "hlcweapons_MG3s"
+            "rhs_c_weapons"
         };
     };
 };
@@ -23,14 +21,6 @@ class CfgWeapons {
         recoil = "recoil_mk200";
     };
     class rhs_weap_pkm : rhs_weap_pkp {
-        recoil = "recoil_mk200";
-    };
-
-    class Rifle_Base_F;
-    class hlc_M60e4_base : Rifle_Base_F {
-        recoil = "recoil_SAW";
-    };
-    class hlc_MG42_base : Rifle_Base_F {
         recoil = "recoil_mk200";
     };
 };
