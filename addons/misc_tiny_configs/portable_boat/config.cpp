4f9674f0907c14a6a2cb034d1398103936fa4e29
freghar <freghar@dummy.tld>
2020-07-03 03:49:39 +0200

    mtc/portable_boat: prevent player death on assembly
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/misc_tiny_configs/portable_boat/config.cpp
+++ addons/misc_tiny_configs/portable_boat/config.cpp
@@ -14,7 +14,10 @@ class CfgPatches {
 };
 
 class CfgVehicles {
-    class B_Boat_Transport_01_F;
+    class Rubber_duck_base_F;
+    class B_Boat_Transport_01_F : Rubber_duck_base_F {
+        class EventHandlers;
+    };
     class a3aa_mtc_portable_boat_boat : B_Boat_Transport_01_F {
         scope = 2;
         displayName = "Portable Boat";
@@ -24,6 +27,12 @@ class CfgVehicles {
             assembleTo = "";
             displayName = "";
             dissasembleTo[] = {"a3aa_portable_boat_backpack"};
+        };
+        class EventHandlers : EventHandlers {
+            class a3aa_mtc_portable_boat {
+                /* turn the boat so it doesn't kill the player by collision */
+                init = "(_this select 0) setDir (getDir (_this select 0) + 270)";
+            };
         };
     };
 
@@ -35,7 +44,7 @@ class CfgVehicles {
         model = "\A3\Drones_F\Weapons_F_Gamma\Ammoboxes\Bags\UAV_backpack_F.p3d";
         hiddenSelectionsTextures[] = {"\A3\Drones_F\Weapons_F_Gamma\Ammoboxes\Bags\data\UAV_backpack_rgr_co.paa"};
         maximumLoad = 0;
-        mass = 600;
+        mass = 300;  /* like UAV backpack */
         mapSize = 0.6;
         class assembleInfo {
             primary = 1;
