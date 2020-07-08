b262791c5820cd2bd5655879e12a36f228b8ecd4
freghar <freghar@dummy.tld>
2020-07-09 01:16:32 +0200

    mtc/sight_alignment: make iron sight alignment slower
    Signed-off-by: freghar <freghar@dummy.tld>

---
+++ addons/misc_tiny_configs/sight_alignment/config.cpp
@@ -0,0 +1,43 @@
+/*
+ * sight alignment - make iron sight (and red dot, etc.) alignment slower
+ *
+ * since Arma 3 simulates sight alignment, these imperfections manifest as
+ * inaccuracy on 300+ meters, helping fix the iron sight sniper problem
+ */
+
+class CfgPatches {
+    class a3aa_mtc_sight_alignment {
+        units[] = {};
+        weapons[] = {};
+        magazines[] = {};
+        requiredAddons[] = {
+            "A3_Data_F"
+        };
+    };
+};
+
+class CfgWeaponHandling {
+    class SightAlignment {
+        /*
+         * maximum misalignment from a perfect axis between eye and target
+         * (higher = larger misalignment)
+         */
+        maximum = 0.8;  // default: 0.7
+        /*
+         * how quickly is the misalignment corrected when not moving mouse
+         * (higher = faster recovery)
+         */
+        decay = 0.6;  // default: 1.8
+        /*
+         * how quickly is the alignment thrown off (ie. by small mouse moves),
+         * eg. how resistant the alignment is to motion
+         * (higher = faster misalignment)
+         */
+        rate = 1;  // default: 0.1
+        /*
+         * how much worse the effect is when looking down sights vs hip-aiming
+         * (multiplier, higher = worse)
+         */
+        //gunnerCoef = 0.2;  // default: 0.2
+    };
+};
