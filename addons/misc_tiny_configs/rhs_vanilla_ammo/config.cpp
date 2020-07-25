ba6d5e83d8744a14108b0e79bd208eed49600414
freghar <freghar@dummy.tld>
2020-07-25 18:03:18 +0200

    mtc/rhs_vanilla_ammo: add flares
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/misc_tiny_configs/rhs_vanilla_ammo/config.cpp
+++ addons/misc_tiny_configs/rhs_vanilla_ammo/config.cpp
@@ -34,4 +34,21 @@ class CfgMagazines {
     class rhs_GRD40_White : rhs_VOG25 { ammo = "G_40mm_Smoke"; };
     class rhs_GRD40_Green : rhs_GRD40_White { ammo = "G_40mm_SmokeGreen"; };
     class rhs_GRD40_Red : rhs_GRD40_White { ammo = "G_40mm_SmokeRed"; };
+    /* UGL usaf flares */
+    //class rhs_mag_M441_HE;
+    class rhs_mag_M585_white : rhs_mag_M441_HE { ammo = "F_40mm_White"; };
+    class rhs_mag_m661_green : rhs_mag_M585_white { ammo = "F_40mm_White"; };
+    class rhs_mag_m662_red : rhs_mag_M585_white { ammo = "F_40mm_Red"; };
+    /* UGL afrf flares */
+    //class rhs_VOG25;
+    class rhs_VG40OP_white : rhs_VOG25 { ammo = "F_40mm_White"; };
+    class rhs_VG40OP_green : rhs_VG40OP_white { ammo = "F_40mm_Green"; };
+    class rhs_VG40OP_red : rhs_VG40OP_white { ammo = "F_40mm_Red"; };
+    /* pistol slot afrf hand-held flares */
+    class UGL_FlareWhite_F;
+    class rhs_mag_rsp30_white : UGL_FlareWhite_F { ammo = "F_40mm_White"; };
+    class UGL_FlareGreen_F;
+    class rhs_mag_rsp30_green : UGL_FlareGreen_F { ammo = "F_40mm_Green"; };
+    class UGL_FlareRed_F;
+    class rhs_mag_rsp30_red : UGL_FlareRed_F { ammo = "F_40mm_Red"; };
 };
