2fc5348e934f728616ceeaee78733cb4b2eeb8e1
Highway <milanimilivoj@gmail.com>
2020-12-19 18:29:09 +0100

    #37-Missing $PBOPREFIX$

--- tools/Build-Pbo.ps1
+++ tools/Build-Pbo.ps1
@@ -1,11 +1,25 @@
 . $PSScriptRoot\Setup-Tools.ps1
 
-if (!(Test-Path $destination\addons)) {
-    mkdir $destination\addons
+if (Test-Path $destination\addons) {
+    Remove-Item -Path $destination\addons -Force -Recurse -Confirm:$false
 }
+
+mkdir $destination\addons
+
+if (Test-Path $destination\optionals) {
+    Remove-Item -Path $destination\optionals -Force -Recurse -Confirm:$false
+}
+
+mkdir $destination\optionals
 
 Get-ChildItem $PSScriptRoot\..\addons | ForEach-Object {
     $outputName = "$destination\addons\" + $_.Name + ".pbo"
+    Write-Output ("Packing " + $_.Name + "to $outputName.")
+    & $addonBuilder "build" "-p" $_.FullName $outputName "--force"
+}
+
+Get-ChildItem $PSScriptRoot\..\optionals | ForEach-Object {
+    $outputName = "$destination\optionals\" + $_.Name + ".pbo"
     Write-Output ("Packing " + $_.Name + "to $outputName.")
     & $addonBuilder "build" "-p" $_.FullName $outputName "--force"
 }
